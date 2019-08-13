f0da6a5f3d64c6ab0bf9ecee2e4707c634a9ff1b
Jacqueline Remmel and Lauren Musante <lauren@>
2019-08-13 15:37:08 -0700

    Add click functions.

---
+++ js/scripts.js
@@ -0,0 +1,39 @@
+$(document).ready(function() {
+
+  $("#a1").click(function() {
+    $("#a1").addClass("green");
+  });
+
+  $("#a2").click(function() {
+    $("#a2").addClass("blue");
+  });
+
+  $("#a3").click(function() {
+    $("#a3").addClass("red");
+  });
+
+  $("#b1").click(function() {
+    $("#b1").addClass("red");
+  });
+
+  $("#b2").click(function() {
+    $("#b2").addClass("yellow");
+  });
+
+  $("#b3").click(function() {
+    $("#b3").addClass("blue");
+  });
+
+  $("#c1").click(function() {
+    $("#c1").addClass("yellow");
+  });
+
+  $("#c2").click(function() {
+    $("#c2").addClass("green");
+  });
+
+  $("#c3").click(function() {
+    $("#c3").addClass("surprise");
+  });
+
+});
